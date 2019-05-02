2519ae4064a3237c7395fbb84c715ac864f73bc7
James Kent <james-kent@uiowa.edu>
2019-05-01 23:50:10 -0500

    read data from Raw

--- Bids/code/Test2PitchToBids.py
+++ Bids/code/Test2PitchToBids.py
@@ -11,35 +11,42 @@ pitch_dir = os.path.dirname(os.path.dirname(script_path))
 
 path = os.path.join(pitch_dir, 'Raw')
 out_dir = os.path.join(pitch_dir, 'Bids')
-beh = glob(os.path.join(path, 'Beh/*[DurationRT]*.txt'))
-translation_dict = {'condition': {'E': 'Experimental', 'C': 'Control'}, 'run_id': {'A': '1', 'B': '2'}, 'session': {'1': 'Pre', '2': 'Post'}}
+beh = glob(os.path.join(path, 'Beh/*BOL_*[DurationRT]*.txt'))
+translation_dict = {'condition': {'E': 'Experimental', 'C': 'Control'},
+                    'run_id': {'A': '1', 'B': '2'},
+                    'session': {'1': 'Pre', '2': 'Post'},
+                   }
 
 # rough draft
 # Optionally match 'ALL'
-pattern = re.compile(r"^.*$")#Raw/Beh/P(?P<subject_id>[0-9]{2})(?P<condition>[CE])(?P<session>[1-2])BOL_(?P<trial_type>[IncCogruetNal]+)_[InCorect]+_(?P<run_id>[AB])_[DurationRT]+.txt$")
-beh_dict = {}
+pattern = re.compile((r"^.*Raw/Beh/P(?P<subject_id>[0-9]{2})"
+                      r"(?P<condition>[CE])"
+                      r"(?P<session>[1-2])BOL_"
+                      r"(?P<trial_type>[IncCogruetNal]+)_"
+                      r"(?P<acc>[InCorect]+)_"
+                      r"(?P<run_id>[AB])_"
+                      r"[DurationRT]+.txt$"))
+cond_dict = {}
 for filename in beh:
     res = re.search(pattern, filename)
-    dict_key = '_'.join(res.groups()[:-1])
-    if dict_key in beh_dict:
-        beh_dict[dict_key].append(filename)
+    if res is None:
+        print("{fname} does not match pattern".format(fname=filename))
+        continue
+    res_dict = res.groupdict()
+    for key in translation_dict.keys():
+        res_dict[key] = translation_dict[key][res_dict[key]]
+    
+    beh_keys = ['subject_id', 'condition', 'session', 'run_id']
+    groups = [res_dict[bkey] for bkey in beh_keys]
+    sub_cond_ses_run = '_'.join(groups)
+    cond_corr = '_'.join([res_dict['trial_type'], res_dict['acc']])
+    if sub_cond_ses_run not in cond_dict:
+        cond_dict[sub_cond_ses_run] = {cond_corr: [filename]}
     else:
-        beh_dict[dict_key] = [filename]
-
-cond_pattern = re.compile(r"^.*beh/.*?([NCIa-z]*)_([ICa-z]*)_[A-Z]_[RTDa-z]*.txt$")
-cond_dict = {}
-for sub_stype_ses_run, fnames in beh_dict.items():
-    for f in fnames:
-        find_cond = re.search(cond_pattern, f)
-        cond_corr = '_'.join(find_cond.groups())
-        if sub_stype_ses_run in cond_dict:
-            if cond_corr in cond_dict[sub_stype_ses_run]:
-                cond_dict[sub_stype_ses_run][cond_corr].append(f)
-            else:
-                cond_dict[sub_stype_ses_run][cond_corr] = [f]
+        if cond_corr not in cond_dict[sub_cond_ses_run]:
+            cond_dict[sub_cond_ses_run][cond_corr] = [filename]
         else:
-            cond_dict[sub_stype_ses_run] = {cond_corr: [f]}
-
+            cond_dict[sub_cond_ses_run][cond_corr].append(filename)
 
 def non_zero_file(fpath):
     return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
@@ -95,7 +102,7 @@ for sub_stype_ses_run, fdict in cond_dict.items():
     run_df = run_df[headers]
     run_df.sort_values(by=['onset'], inplace=True)
     sub, stype, ses, run = sub_stype_ses_run.split('_')
-    out_file_dir = os.path.join(os.getcwd(), 'Bids/sub-0{sub}/ses-{stype}{ses}/func/'.format(sub=sub, ses=ses, stype=stype))
+    out_file_dir = os.path.join(out_dir, 'sub-0{sub}/ses-{stype}{ses}/func/'.format(sub=sub, ses=ses, stype=stype))
     os.makedirs(out_file_dir, exist_ok=True)
     out_file = os.path.join(out_file_dir, 'sub-0{sub}_ses-{stype}{ses}_task-flanker_run-0{run}_events.tsv'.format(sub=sub, ses=ses, run=run, stype=stype))
     if not run_df.empty:
